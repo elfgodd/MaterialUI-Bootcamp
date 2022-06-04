0bd30364c59154a20cd1dc5ef07ef0027c24f8ae
ElfGod <albertogx@icloud.com>
2022-06-04 00:02:01 -0500

    Styling - Inline

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
 function ElevationScroll(props) {
   const { children } = props
@@ -16,15 +17,25 @@ function ElevationScroll(props) {
   })
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}))
+
 const Header = (props) => {
+  const classes = useStyles()
   return (
-    <ElevationScroll>
-      <AppBar position='fixed'>
-        <Toolbar>
-          <Typography variant='h3'>Arc Development</Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar position='fixed'>
+          <Toolbar>
+            <Typography variant='h3'>Arc Development</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin}></div>
+    </>
   )
 }
 
