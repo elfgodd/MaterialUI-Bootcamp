69b7b47680e86b156f43e4e8c36ca33109159191
ElfGod <albertogx@icloud.com>
2022-06-16 18:04:21 -0500

    Styling The Hero Block Continued

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Lottie from 'react-lottie'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import ButtonArrow from '../components/ui/ButtonArrow'
 import Typography from '@material-ui/core/Typography'
 
 import animationData from '../animations/landinganimation/data'
+import { BlockRounded } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -14,11 +15,57 @@ const useStyles = makeStyles((theme) => ({
     minWidth: '21em',
     marginTop: '2em',
     marginLeft: '10%',
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: '30em',
+    },
+  },
+  estimateButton: {
+    ...theme.typography.estimate,
+    backgroundColor: theme.palette.common.orange,
+    borderRadius: '50px',
+    height: 45,
+    width: 145,
+    marginRight: 40,
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light,
+    },
+  },
+  buttonContainer: {
+    marginTop: '20em',
+  },
+  learnButtonHero: {
+    borderColor: theme.palette.common.blue,
+    color: theme.palette.common.blue,
+    borderWidth: 2,
+    textTransform: 'none',
+    borderRadius: '50px',
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: '0.9rem',
+    height: 45,
+    width: 145,
+  },
+  mainContainer: {
+    marginTop: '5em',
+    [theme.breakpoints.down('md')]: {
+      marginTop: '3em',
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '2em',
+    },
+  },
+  heroTextContainer: {
+    minWidth: '21.5em',
+    marginLeft: '1em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
   },
 }))
 
 export default function LandingPage() {
   const classes = useStyles()
+  const theme = useTheme()
 
   const defaultOptions = {
     loop: true,
@@ -30,7 +77,7 @@ export default function LandingPage() {
   }
 
   return (
-    <Grid container direction='column'>
+    <Grid container direction='column' className={classes.mainContainer}>
       <Grid item>
         <Grid
           container
@@ -38,20 +85,30 @@ export default function LandingPage() {
           alignItems='center'
           direction='row'
         >
-          <Grid sm item>
+          <Grid sm item className={classes.heroTextContainer}>
             <Typography variant='h2' align='center'>
               Bringing West Coast Technology
               <br />
               to the Midwest
             </Typography>
-            <Grid container>
+            <Grid
+              container
+              justifyContent='center'
+              classesName={classes.buttonContainer}
+            >
               <Grid item>
-                <Button variant='contained'>Free Estimate</Button>
+                <Button className={classes.estimateButton} variant='contained'>
+                  Free Estimate
+                </Button>
               </Grid>
               <Grid item>
-                <Button variant='outlined'>
-                  Learn More
-                  <ButtonArrow width={15} height={15} fill='red' />
+                <Button variant='outlined' className={classes.learnButtonHero}>
+                  <span style={{ marginRight: 10 }}>Learn More</span>
+                  <ButtonArrow
+                    width={15}
+                    height={15}
+                    fill={theme.palette.common.blue}
+                  />
                 </Button>
               </Grid>
             </Grid>
